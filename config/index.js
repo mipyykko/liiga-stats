de3ee6bc34f180e8864cd3e929e12f3b45618373
Mikko Pyykkö <mikko.pyykko@helsinki.fi>
2019-04-26 17:50:12 +0300

    api uri to .env

--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@ const config = {
   PORT: process.env.PORT || 3001,
   MONGO_URI: process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/liiga',
   
-  INSTAT_API_URI: 'http://mc.instatfootball.com/api/v1/',
+  API_URI: process.env.API_URI,
   LOCAL_DATA_DIRECTORY: process.env.LOCAL_DATA_DIRECTORY
 }
 
